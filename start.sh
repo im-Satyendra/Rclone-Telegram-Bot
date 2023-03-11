2475f032722a81b39e2c2170cf6ece6004f80713
SaTyEndRa <84174959+im-Satyendra@users.noreply.github.com>
2023-03-11 16:10:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-
+curl https://rclone.org/install.sh | sudo bash
+pip3 install -r requirements.txt
+pip install python-telegram-bot --upgrade
+pip install subprocess32
+pip install subprocess.run
+wget https://gist.githubusercontent.com/im-Satyendra/01f71e2819b838382619f759de18f913/raw/1d795c3ebf0884c05a47a946e45add89f301d4ff/rclone.conf
+python bot.py
